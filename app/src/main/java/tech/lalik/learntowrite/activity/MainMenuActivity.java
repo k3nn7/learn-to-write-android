27558a1754fe111241b13858dd00685067b116dd
Łukasz Lalik <lukasz.lalik@brainly.com>
2020-01-14 20:54:41 +0100

    Add activities with menus

---
+++ app/src/main/java/tech/lalik/learntowrite/activity/MainMenuActivity.java
@@ -0,0 +1,16 @@
+package tech.lalik.learntowrite.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tech.lalik.learntowrite.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
---
+++ app/src/main/java/tech/lalik/learntowrite/activity/TakeTestMenuActivity.java
@@ -0,0 +1,16 @@
+package tech.lalik.learntowrite.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tech.lalik.learntowrite.R;
+
+public class TakeTestMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_test_menu);
+    }
+}
