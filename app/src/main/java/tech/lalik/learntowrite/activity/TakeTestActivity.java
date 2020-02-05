b0b17e54b8183272624e0824b823e2aa6971fa54
Łukasz Lalik <lukasz.lalik@brainly.com>
2020-02-05 07:50:02 +0100

    Use BinaryBitmap logic for comparing letter

--- app/src/main/java/tech/lalik/learntowrite/activity/TakeTestActivity.java
+++ app/src/main/java/tech/lalik/learntowrite/activity/TakeTestActivity.java
@@ -2,10 +2,8 @@ package tech.lalik.learntowrite.activity;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -16,6 +14,8 @@ import java.util.concurrent.ExecutionException;
 
 import tech.lalik.learntowrite.LetterCanvas;
 import tech.lalik.learntowrite.R;
+import tech.lalik.learntowrite.bitmap.BinaryBitmap;
+import tech.lalik.learntowrite.bitmap.BoundingBox;
 
 public class TakeTestActivity extends AppCompatActivity {
     private LetterCanvas letterCanvas;
@@ -29,33 +29,6 @@ public class TakeTestActivity extends AppCompatActivity {
 
         letterCanvas = findViewById(R.id.letterCanvas);
         imageView = findViewById(R.id.imageWithLetter);
-        imageView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    Bitmap templateLetter = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
-
-
-                    int pixel = templateLetter.getPixel((int) event.getX(), (int) event.getY());
-                    String color = String.format(
-                            "(%d, %d, %d, %d)",
-                            Color.alpha(pixel),
-                            Color.red(pixel),
-                            Color.green(pixel),
-                            Color.blue(pixel)
-                    );
-
-                    Toast toast = Toast.makeText(
-                            getApplicationContext(),
-                            String.format("(%f, %f) %s", event.getX(), event.getY(), color),
-                            Toast.LENGTH_LONG
-                    );
-                    toast.show();
-                }
-
-                return true;
-            }
-        });
     }
 
     public void onClear(View view) {
@@ -76,48 +49,55 @@ public class TakeTestActivity extends AppCompatActivity {
 
         double result = new CompareTask().execute(templateLetter, writtenLetter).get();
         Toast toast2 = Toast.makeText(getApplicationContext(), String.format("Difference %f", result), Toast.LENGTH_LONG);
-        toast2.show();
         letterCanvas.invalidate();
+        toast2.show();
     }
 
-    class CompareTask extends AsyncTask<Bitmap, Void, Double> {
+    static class CompareTask extends AsyncTask<Bitmap, Void, Double> {
         @Override
         protected Double doInBackground(Bitmap... bitmaps) {
             Bitmap templateLetter = bitmaps[0];
             Bitmap writtenLetter = bitmaps[1];
 
-            int width = Math.min(templateLetter.getWidth(), writtenLetter.getWidth());
-            int height = Math.min(templateLetter.getHeight(), writtenLetter.getHeight());
-            double diff = 0.0;
-
-            for (int i = 0; i < width; i++) {
-                for (int j = 0; j < height; j++) {
-                    int templatePixel = templateLetter.getPixel(i, j);
-                    int letterPixel = writtenLetter.getPixel(i, j);
-
-                    int templateColor = 1;
-                    if (Color.alpha(templatePixel) == 255) {
-                        templateColor = 0;
+            BinaryBitmap templateLetterBitmap = new BinaryBitmap(templateLetter.getWidth(), templateLetter.getHeight());
+            for (int i = 0; i < templateLetter.getWidth(); i++) {
+                for (int j = 0; j < templateLetter.getHeight(); j++) {
+                    if (templateLetter.getPixel(i, j) == Color.BLACK) {
+                        templateLetterBitmap.turnOnPixel(i, j);
                     }
-
-                    int letterColor = 1;
-                    if (Color.red(letterPixel) == 0) {
-                        letterColor = 0;
-                    }
-
-                    if (templateColor == 0 && letterColor == 0) {
-                        writtenLetter.setPixel(i, j, Color.rgb(0, 255, 0));
-                    }
-
-                    if (templateColor == 0 && letterColor != 0) {
-                        writtenLetter.setPixel(i, j, Color.rgb(255, 0, 0));
-                    }
-
-                    diff += Math.pow(templateColor - letterColor, 2);
                 }
             }
 
-            return diff / (width * height);
+            BinaryBitmap writtenLetterBitmap = new BinaryBitmap(writtenLetter.getWidth(), writtenLetter.getHeight());
+            for (int i = 0; i < writtenLetter.getWidth(); i++) {
+                for (int j = 0; j < templateLetter.getHeight(); j++) {
+                    if (writtenLetter.getPixel(i, j) == Color.BLACK) {
+                        writtenLetterBitmap.turnOnPixel(i, j);
+                    }
+                }
+            }
+
+            BoundingBox templateBoundingBox = templateLetterBitmap.getBoundingBox();
+            BoundingBox writtenLetterBoundingBox = writtenLetterBitmap.getBoundingBox();
+
+            writtenLetterBitmap.translate(
+                    templateBoundingBox.left - writtenLetterBoundingBox.left,
+                    templateBoundingBox.top - writtenLetterBoundingBox.top
+            );
+
+            writtenLetterBoundingBox = writtenLetterBitmap.getBoundingBox();
+            writtenLetterBitmap.scaleBox(writtenLetterBoundingBox, templateBoundingBox);
+
+            writtenLetter.eraseColor(Color.WHITE);
+            for (int i = 0; i < writtenLetter.getWidth(); i++) {
+                for (int j = 0; j < writtenLetter.getHeight(); j++) {
+                    if (writtenLetterBitmap.getPixel(i, j)) {
+                        writtenLetter.setPixel(i, j, Color.BLACK);
+                    }
+                }
+            }
+
+            return templateLetterBitmap.distanceToBitmap(writtenLetterBitmap);
         }
     }
 }
